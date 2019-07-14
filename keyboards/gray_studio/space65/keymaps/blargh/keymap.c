328e5c555b177affd14a4b242c67516cd6db1ed0
arghblargh <elsonchin27@gmail.com>
2019-07-14 13:03:00 -0700

    update space65

--- keyboards/gray_studio/space65/keymaps/blargh/keymap.c
+++ keyboards/gray_studio/space65/keymaps/blargh/keymap.c
@@ -38,10 +38,10 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     ),
 [2] = LAYOUT( \
         KC_GRV,     KC_F1,      KC_F2,      KC_F3,      KC_F4,      KC_F5,      KC_F6,      KC_F7,      KC_F8,      KC_F9,      KC_F10,     KC_F11,     KC_F12,     KC_TRNS,    KC_TRNS,    KC_TRNS,    \
-        S(KC_GRV),              KC_TRNS,    KC_TRNS,    KC_TRNS,    KC_TRNS,    KC_TRNS,    KC_P7,      KC_P8,      KC_P9,      KC_NLCK,    KC_TRNS,    KC_TRNS,    KC_TRNS,    KC_TRNS,    KC_TRNS,    \
-        KC_TRNS,                KC_TRNS,    KC_TRNS,    KC_TRNS,    KC_TRNS,    KC_TRNS,    KC_P4,      KC_P5,      KC_P6,      KC_TRNS,    KC_TRNS,    KC_TRNS,    KC_TRNS,                KC_TRNS,    \
-        KC_TRNS,    KC_TRNS,    KC_TRNS,    KC_TRNS,    KC_TRNS,    KC_TRNS,    KC_P0,      KC_P1,      KC_P2,      KC_P3,      KC_TRNS,    KC_TRNS,    KC_TRNS,                KC_TRNS,    KC_TRNS,    \
-        KC_TRNS,    KC_TRNS,    KC_TRNS,                            KC_TRNS,    KC_TRNS,    KC_TRNS,                KC_TRNS,    KC_TRNS,                            KC_TRNS,    KC_TRNS,    KC_TRNS     \
+        S(KC_GRV),              KC_TRNS,    KC_TRNS,    KC_TRNS,    KC_TRNS,    KC_TRNS,    KC_TRNS,    KC_P7,      KC_P8,      KC_P9,      KC_NLCK,    KC_TRNS,    KC_TRNS,    KC_TRNS,    KC_TRNS,    \
+        KC_TRNS,                KC_TRNS,    KC_TRNS,    KC_TRNS,    KC_TRNS,    KC_TRNS,    KC_TRNS,    KC_P4,      KC_P5,      KC_P6,      KC_TRNS,    KC_TRNS,    KC_TRNS,                KC_TRNS,    \
+        KC_TRNS,    KC_TRNS,    KC_TRNS,    KC_TRNS,    KC_TRNS,    KC_TRNS,    KC_TRNS,    KC_TRNS,    KC_P1,      KC_P2,      KC_P3,      KC_PDOT,    KC_TRNS,                KC_TRNS,    KC_TRNS,    \
+        KC_TRNS,    KC_TRNS,    KC_TRNS,                            KC_TRNS,    KC_TRNS,    KC_P0,                  KC_TRNS,    KC_TRNS,                            KC_TRNS,    KC_TRNS,    KC_TRNS     \
     ),
 };
 
