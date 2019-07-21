2e7f717fec17dcb8e2feb4735cf5138dc2ccd433
arghblargh <elsonchin27@gmail.com>
2019-07-21 00:41:46 -0700

    doro67 keymap

--- keyboards/doro67/multi/keymaps/blargh/keymap.c
+++ keyboards/doro67/multi/keymaps/blargh/keymap.c
@@ -1,4 +1,4 @@
-/* Copyright 2019 MechMerlin
+/* Copyright 2019 ShadeDream
  *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
@@ -16,41 +16,25 @@
 #include QMK_KEYBOARD_H
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
-[0] = LAYOUT( \
+[0] = LAYOUT_multi( \
         KC_GESC,    KC_1,       KC_2,       KC_3,       KC_4,       KC_5,       KC_6,       KC_7,       KC_8,       KC_9,       KC_0,       KC_MINS,    KC_EQL,     KC_BSPC,    KC_DEL,     KC_HOME,    \
         KC_TAB,                 KC_Q,       KC_W,       KC_E,       KC_R,       KC_T,       KC_Y,       KC_U,       KC_I,       KC_O,       KC_P,       KC_LBRC,    KC_RBRC,    KC_BSLS,    KC_PGUP,    \
         KC_LCTL,                KC_A,       KC_S,       KC_D,       KC_F,       KC_G,       KC_H,       KC_J,       KC_K,       KC_L,       KC_SCLN,    KC_QUOT,    KC_ENT,                 KC_PGDN,    \
         KC_LSFT,    KC_LSFT,    KC_Z,       KC_X,       KC_C,       KC_V,       KC_B,       KC_N,       KC_M,       KC_COMM,    KC_DOT,     KC_SLSH,    KC_RSFT,                KC_UP,      KC_END,     \
-        KC_LCTL,    KC_LGUI,    KC_LALT,                            KC_SPC,     MO(2),      KC_BSPC,                MO(1),      KC_RCTL,                            KC_LEFT,    KC_DOWN,    KC_RGHT     \
+        KC_LCTL,    KC_LGUI,    KC_LALT,                            KC_SPC,     MO(2),      KC_BSPC,                MO(1),      KC_RCTL,    KC_NO,                  KC_LEFT,    KC_DOWN,    KC_RGHT     \
     ),
-[1] = LAYOUT( \
+[1] = LAYOUT_multi( \
         RESET,      KC_F1,      KC_F2,      KC_F3,      KC_F4,      KC_F5,      KC_F6,      KC_F7,      KC_F8,      KC_F9,      KC_F10,     KC_F11,     KC_F12,     KC_PSCR,    KC_SLCK,    KC_PAUS,    \
         KC_TRNS,                RGB_TOG,    RGB_MOD,    RGB_HUI,    RGB_HUD,    RGB_SAI,    RGB_SAD,    RGB_VAI,    RGB_VAD,    KC_TRNS,    KC_TRNS,    KC_TRNS,    KC_TRNS,    KC_TRNS,    KC_TRNS,    \
         KC_CAPS,                BL_TOGG,    BL_INC,     BL_DEC,     BL_STEP,    KC_TRNS,    KC_TRNS,    KC_TRNS,    KC_TRNS,    G(KC_L),    KC_TRNS,    KC_TRNS,    KC_TRNS,                KC_INS,     \
         KC_TRNS,    KC_TRNS,    KC_TRNS,    KC_TRNS,    KC_TRNS,    KC_TRNS,    KC_TRNS,    KC_TRNS,    KC_TRNS,    KC_TRNS,    KC_TRNS,    KC_MSTP,    KC_MPLY,                KC_VOLU,    KC_MUTE,    \
-        KC_TRNS,    KC_TRNS,    KC_TRNS,                            KC_TRNS,    KC_TRNS,    KC_TRNS,                KC_TRNS,    KC_TRNS,                            KC_MPRV,    KC_VOLD,    KC_MNXT     \
+        KC_TRNS,    KC_TRNS,    KC_TRNS,                            KC_TRNS,    KC_TRNS,    KC_TRNS,                KC_TRNS,    KC_TRNS,    KC_NO,                  KC_MPRV,    KC_VOLD,    KC_MNXT     \
     ),
-[2] = LAYOUT( \
+[2] = LAYOUT_multi( \
         KC_GRV,     KC_F1,      KC_F2,      KC_F3,      KC_F4,      KC_F5,      KC_F6,      KC_F7,      KC_F8,      KC_F9,      KC_F10,     KC_F11,     KC_F12,     KC_TRNS,    KC_TRNS,    KC_TRNS,    \
         KC_TRNS,                KC_TRNS,    KC_TRNS,    KC_TRNS,    KC_TRNS,    KC_TRNS,    KC_TRNS,    KC_P7,      KC_P8,      KC_P9,      KC_NLCK,    KC_TRNS,    KC_TRNS,    KC_TRNS,    KC_TRNS,    \
         KC_TRNS,                KC_TRNS,    KC_TRNS,    KC_TRNS,    KC_TRNS,    KC_TRNS,    KC_TRNS,    KC_P4,      KC_P5,      KC_P6,      KC_TRNS,    KC_TRNS,    KC_TRNS,                KC_TRNS,    \
         KC_TRNS,    KC_TRNS,    KC_TRNS,    KC_TRNS,    KC_TRNS,    KC_TRNS,    KC_TRNS,    KC_TRNS,    KC_P1,      KC_P2,      KC_P3,      KC_PDOT,    KC_TRNS,                KC_TRNS,    KC_TRNS,    \
-        KC_TRNS,    KC_TRNS,    KC_TRNS,                            KC_TRNS,    KC_TRNS,    KC_P0,                  KC_TRNS,    KC_TRNS,                            KC_TRNS,    KC_TRNS,    KC_TRNS     \
+        KC_TRNS,    KC_TRNS,    KC_TRNS,                            KC_TRNS,    KC_TRNS,    KC_P0,                  KC_TRNS,    KC_TRNS,    KC_NO,                  KC_TRNS,    KC_TRNS,    KC_TRNS     \
     ),
 };
-
-bool process_record_user(uint16_t keycode, keyrecord_t *record) {
-  return true;
-}
-
-void matrix_init_user(void) {
-
-}
-
-void matrix_scan_user(void) {
-
-}
-
-void led_set_user(uint8_t usb_led) {
-
-}
